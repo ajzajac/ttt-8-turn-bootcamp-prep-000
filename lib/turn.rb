141a03a49cfa617f1fa7601d0afc0242a9966904
Andrew Zajac <ajzajac@icloud.com>
2019-07-30 19:46:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,10 @@ def display_board(board)
  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board, index)
+  index.between?(0, 8) && !(position_taken?(board, index))
+end
+
+def position_taken?(board, index)
+  ! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
+end
